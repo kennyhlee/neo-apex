4e573229c7c3b92b1ea8299654ead302749476a1
Kenny Lee <kennyhlee@gmail.com>
2026-08-04 04:16:39 -0700

    feat(flow-runtime): export flowT, validateFlowField, blockConfig accessors, formatCents from barrel

--- flow-runtime/src/index.ts
+++ flow-runtime/src/index.ts
@@ -1,2 +1,6 @@
 export * from './types';
 export { FlowRenderer, type FlowRendererProps } from './FlowRenderer';
+export { flowT } from './i18n';
+export { validateFlowField } from './validateField';
+export { formFields, docsOf, plansOf, planAmounts, messageBody } from './blockConfig';
+export { formatCents } from './money';
